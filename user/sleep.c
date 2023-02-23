68eae94b8da4f365cb7a94dbcf8150967e0cb68c
ruitianzhong <zhongruitian2003@qq.com>
2023-02-23 13:30:24 +0800

    finish the sleep

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(2, "Usage:sleep time\n");
+        exit(1);
+    }
+    if (sleep(atoi(argv[1])) < 0)
+    {
+        fprintf(2, "sleep failed\n");
+    }
+    exit(0);
+}
